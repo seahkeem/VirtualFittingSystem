91ce9006b3fbe7a3c6bd28ca4f08f945a254b768
Seah Kim <seahkeem@gmail.com>
2026-03-26 11:46:06 +0900

    chore: cleanup unused files

--- virtualFittingApp/src/shared/components/logo3D/index.ts
+++ virtualFittingApp/src/shared/components/logo3D/index.ts
@@ -3,4 +3,3 @@ export * from "./ui/Basilium3DLogoMain";
 export * from "@/shared/components/logo3D/ui/PopLogo";
 export * from "@/shared/components/logo3D/ui/BasiliumNoAnimationLogo ";
 export * from "@/shared/components/logo3D/ui/BasiliumSVGLogo";
-export * from "@/shared/components/logo3D/ui/Basilium3DModelLogin";
